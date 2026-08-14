351d80337ae525c3faa9da0f15d5d0c9fb0bd41a
rahilcodes <rahilcodes@users.noreply.github.com>
2026-08-14 17:45:16 +0530

    Add Meta Pixel tracking code (Pixel ID: 2401243337708838)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,32 @@ export default function RootLayout({
             })
           }}
         />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2401243337708838');
+fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2401243337708838&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={`${inter.variable} font-sans antialiased text-black bg-white`} suppressHydrationWarning>
         <Navbar />
